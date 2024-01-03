a915cb1b732d6885c871245b1fc8cf9014e2c799
NannoHouse <gergana.yordanovaa03@gmail.com>
2024-01-03 23:18:35 +0200

    Deleted files

--- test1/task8.py
+++
@@ -1,8 +0,0 @@
-def find_shortest_and_longest_words(words, letter):
-    words = [word for word in words if word.startswith(letter)]
-    minn = min(words,key=len)
-    maxx = max(words,key=len)
-    return (minn,maxx)
-
-assert find_shortest_and_longest_words(["abracadabra", "banana", "kugelschreiber", "ant", "almost", "thisshouldbeaverylongword"], "a") == ("ant", "abracadabra")
-assert find_shortest_and_longest_words(["abracadabra", "banana", "kugelschreiber", "ant", "almost", "thisshouldbeaverylongword"], "b") == ("banana", "banana")
